551d8d2d0e78011d35f3574c349d4068f985e407
Giles Excell <9713395@student.swin.edu.au>
2015-07-18 19:23:35 +1000

    Civilian AI

---
+++ grandfatherkiller/Assets/CivilianAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CivilianAI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		NavMeshAgent agent = GetComponent<NavMeshAgent>();
+          
+		if (!agent.hasPath) {
+			Vector2 direction = Random.insideUnitCircle * 5;
+			agent.destination = new Vector3(transform.position.x + direction.x, 0, transform.position.z + direction.y);
+		}
+	}	
+}

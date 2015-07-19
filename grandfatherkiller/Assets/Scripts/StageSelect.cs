551a9cd4ad6246e64d92f2d2f6f560e03937fd75
Giles Excell <9713395@student.swin.edu.au>
2015-07-19 15:25:09 +1000

    Menu stuff, music

--- grandfatherkiller/Assets/Scripts/StageSelect.cs
+++ grandfatherkiller/Assets/Scripts/StageSelect.cs
@@ -3,13 +3,14 @@ using System.Collections;
 using InControl;
 
 public class StageSelect : MonoBehaviour {
-	void update() {
+	void Update() {
 		var inputDevice = InputManager.ActiveDevice;
-
 		if (inputDevice.Action1.WasPressed) {
 			Application.LoadLevel ("Modern");
 		} else if (inputDevice.Action2.WasPressed) {
 			Application.LoadLevel ("Medieval");
+		} else if (inputDevice.MenuWasPressed) {
+			Application.Quit();
 		}
 	}	
 }
